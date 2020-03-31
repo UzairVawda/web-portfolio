376b8aca23ae2866312ea7297abbaf6a1a4fe3b7
umv23 <umv23@cs.drexel.edu>
2020-03-31 09:01:26 -0400

    custom clean up

--- static/js/custom.js
+++
@@ -1,7 +0,0 @@
-$("#send-message").on("click", function(e) {
-    var form = document.getElementById("contact-form");
-    var success = document.getElementById("success-message");
-    form.display = "none";
-    success.display = "inline";
-    console.log('sent');
-});
